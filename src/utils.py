5306105d583525b651ba3e1c60e2878a2a41223d
Shivam singh <79458284+shivam8496@users.noreply.github.com>
2025-02-19 02:01:59 +0530

    Hyperparmeter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from src.exception import CustomException
 import dill
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
 
@@ -17,21 +18,46 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,Y_train,X_test,Y_test,models):
-    try:
-        report={}
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
-            model.fit(X_train,Y_train)
-            Y_pred_train = model.predict(X_train)
-            Y_pred_test = model.predict(X_test)
-            train_score = r2_score(Y_train,Y_pred_train)
-            test_score = r2_score(Y_test,Y_pred_test)
-            report[list(models.keys())[i]]=test_score
-        return report
-    except Exception as e:
-        raise CustomException(e,sys)
-
+class Evaluate:
+    def evaluate_models(self,X_train,Y_train,X_test,Y_test,models):
+        try:
+            report={}
+            for i in range(len(list(models))):
+                model=list(models.values())[i]
+                model.fit(X_train,Y_train)
+                Y_pred_train = model.predict(X_train)
+                Y_pred_test = model.predict(X_test)
+                train_score = r2_score(Y_train,Y_pred_train)
+                test_score = r2_score(Y_test,Y_pred_test)
+                report[list(models.keys())[i]]=test_score
+            return report
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+    def evaluate_models(self,X_train,Y_train,X_test,Y_test,models,params):
+        try:
+            report={}
+            for i in range(len(list(models))):
+                print(f"=========================================")
+                model=list(models.values())[i]
+                param=list(params.values())[i]
+                model_name=list(models.keys())[i]
+                print(f"model==>{model_name} and parameter==>{list(params.keys())[i]}")
+                gs=GridSearchCV(model,param,cv=3)
+                gs.fit(X_train,Y_train)
+                print(f"Fitting into model==>{model_name}")
+                model.set_params(**gs.best_params_)
+                model.fit(X_train,Y_train)
+                Y_pred_train = model.predict(X_train)
+                Y_pred_test = model.predict(X_test)
+                print(f"Predicting for  model==>{model_name} ")
+                train_score = r2_score(Y_train,Y_pred_train)
+                test_score = r2_score(Y_test,Y_pred_test)
+                report[list(models.keys())[i]]=test_score
+                
+            return report
+        except Exception as e:
+            raise CustomException(e,sys)
 
 
 
